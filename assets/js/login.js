fbe75a5049eed852c0d33e8b9951f5cbd9a5fde5
zhouyue <2117660358@qq.com>
2021-03-08 10:25:41 +0800

    login and logup

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,72 +1,75 @@
 $(function(){
-//给登录链接注册点击事件
-$('#link_si').on('click',function(){
-
-$('.regbox').hide()
-$('.loginbox').show()
-
-})//------
-//给注册账号链接注册点击事件
-$('#link_su').on('click',function(){
-    $('.loginbox').hide()
-    $('.regbox').show()
-    })//-------
-
-//自定义校验规则
-layui.form.verify({
- psd: [
-      /^[\S]{6,12}$/
-      ,'密码必须6到12位，且不能出现空格'
-    ] ,
-    repsd: function(value, item){ //value：表单的值、item：表单的DOM对象
-        var psd = $('#psd').val()
-          if(value != psd) {
-              return "两次输入密码不一致"
-          }
-      }
-      
-      
-  });      
-  //监听注册表单的提价事件
-  $('#register').on('submit',function(e){
-  e.preventDefault()
-  var data = {username:$('#register [name=username]').val(),
-password:$('#register [name=password]').val()
+var form = layui.form
+var layer = layui.layer
+//给 link_reg 注册点击事件
+$('#link_reg').on('click',function(){
+//登录盒子隐藏 注册盒子显示
+$('.login-box').hide()
+$(".reg-box").show()
+})
+$('#link_login').on('click',function(){
+    //登录盒子隐藏 注册盒子显示
+    $('.reg-box').hide()
+    $(".login-box").show()
+})
+//表单---验证 提交 获取 展示
+form.verify({
+pwd:[/^[\S]{6,12}$/,'密码须6到12位，且不能有空格'],
+repwd:function(value){
+var firstPwd = $('#firstPwd').val()
+if(firstPwd !== value){
+    return '两次密码不一致，请重新输入'
 }
-  $.post('/api/reguser',data,function(res){
-      if(res.status !== 0) {
-          return layui.layer.msg(res.message)
-      }
-      layui.layer.msg('注册成功')
-      $('#link_si').click()
-  })
-  })
-  //监听登录表单的提交行为
-  $('#login').on('submit',function(e){
-      e.preventDefault()
-      $.ajax({
-          url:"/api/login",
-          method:"POST",
-          data: $(this).serialize(),
-          success: function (res) {
-            if(res.status !== 0) {
-               
-                return layui.layer.msg(res.message)
-            }
-           
-            layui.layer.msg('登录成功')
-            //将返回的token 存入到当前域名下的localstorage中
-            localStorage.setItem('token',res.token)
-            //成功后跳转首页
-            location.href="/index.html"
+}
+})
+//监听注册表单的提交 -- 阻止默认提交行为 ajax 请求
+$('#form_reg').on('submit',function(e){
+e.preventDefault()
+var datas = {
+    username: $('#form_reg [name=username]').val(),
+    password: $('#form_reg [name=password]').val()
+}
+$.ajax({
+    method:'POST',
+    url:"/api/reguser",
+    data:datas,
+    success:function(res){
+        console.log(res);
+        if(res.status !== 0){return layer.msg('注册失败'+ res.message)}
+        layer.msg('注册成功，即将跳转登录页面')
+        $('#link_login').click()
+    }
+})
+})
+//蒋婷登录表单的提交 
+$('#form_login').on('submit',function(e){
+    e.preventDefault()
+    $.ajax({
+        method:'POST',
+        url:'/api/login',
+        data:$(this).serialize(),
+        success:function(res){
+            if(res.status !== 0){return layer.msg('登录失败')}
+        layer.msg('登录成功，即将跳转首页')
+        localStorage.setItem('token',res.token)
+        location.href = '/index.html'
+        }
+    })
+})
+    
 
-          }
-      });
-  })
 
 
 
 
 
-//------------------------ 
+
+
+
+
+
+
+
+
+    //------------------------------
 })
